ccb2abdf9e528957afd984f3dfb7c73f67a8f98c
Iain Riley <iain.riley@gmail.com>
2017-03-26 23:56:31 +0800

    Set chmod permissions

--- scripts/install.js
+++ scripts/install.js
@@ -2,6 +2,6 @@ var fs = require('fs');
 var path = require('path');
 
 if (process.platform === 'linux') {
-    fs.chmodSync(path.join(__dirname, '..', 'bin', 'wmic_centos_x64'), 777);
-    fs.chmodSync(path.join(__dirname, '..', 'bin', 'wmic_ubuntu_x64'), 777);
+    fs.chmodSync(path.join(__dirname, '..', 'bin', 'wmic_centos_x64'), 0755);
+    fs.chmodSync(path.join(__dirname, '..', 'bin', 'wmic_ubuntu_x64'), 0755);
 }
